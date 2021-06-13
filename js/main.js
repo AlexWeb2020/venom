353b1df60d03191f44f52a6326ad17e334b82d5d
AlexWeb2020 <70015066+AlexWeb2020@users.noreply.github.com>
2021-06-13 22:14:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.nav-menu');
+const menuCloseBtn = document.querySelector('.menu-close');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.add('is-active');
+  menuCloseBtn.classList.add('is-active');
+});
+
+menuCloseBtn.addEventListener('click', () => {
+  menu.classList.remove('is-active');
+  menuCloseBtn.classList.remove('is-active');
+});
